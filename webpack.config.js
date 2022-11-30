cdfcf0b1b0e40e62bcd1fcdbf8da673760ee7cec
liangfeng_wang <liangfeng_w@kingdee.com>
2022-11-30 19:21:09 +0800

    feat: add html-webpack-plugin webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,21 @@
-const path = require('path')
-
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, './dist')
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "./dist"),
+    clean: true, // 在生成文件之前清空 output 目录
   },
-  mode: 'production', // production development
-}
+  mode: "development", // production development
+  devtool: 'inline-source-map',
+  plugins: [new HtmlWebpackPlugin({
+    template: './src/index.html',
+    filename: 'app.html',
+    inject: 'body', // 在指定位置去生成script标签
+  })],
+  devServer: {
+    static: './dist'
+  }
+};
